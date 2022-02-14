aa5fee3135b91d0e912c19414ab2c848a7c34a1e
Rifki Ramadhan <rifkiramadhan.9428@gmail.com>
2022-02-15 02:02:40 +0700

    Test Again

--- src/components/display/index.ts
+++ src/components/display/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from "./Header";
-export { default as Header404 } from "./Header404";
+export { default as Header404 } from "./HeaderError";
 export { default as Footer } from "./Footer";
 export { default as Loader } from "./Loader";
 export { default as Breadcrumb } from "./Breadcrumb";
