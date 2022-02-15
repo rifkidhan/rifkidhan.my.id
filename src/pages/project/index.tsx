a4c2594fdc72afa898405c2e818bc6a5e3150645
Rifki Ramadhan <rifkiramadhan.9428@gmail.com>
2022-02-15 09:12:39 +0700

    minor change 0.0.1

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Layout from "@/components/Layout";
 import type { ReactElement } from "react";
 import { NextSeo } from "next-seo";
@@ -29,23 +28,3 @@ export default ProjectPage;
 ProjectPage.getLayout = function getLayout(page: ReactElement) {
 	return <Layout>{page}</Layout>;
 };
-=======
-import Layout from "@/components/Layout";
-import type { ReactElement } from "react";
-import { NextSeo } from "next-seo";
-
-const ProjectPage = () => {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen gap-10">
-			<NextSeo title="My Project" />
-			<div>Project Page</div>
-		</div>
-	);
-};
-
-export default ProjectPage;
-
-ProjectPage.getLayout = function getLayout(page: ReactElement) {
-	return <Layout>{page}</Layout>;
-};
->>>>>>> 9e54861 (Initial Commit)
